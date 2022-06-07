3a6fa4d06f7a7861a0d7cd4891684f018ef3c98a
Nishanthbnish <bnishanthc@gmai.com>
2022-06-07 11:41:15 +0530

    Add project files.

---
+++ Jsontoxml/Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Xml;
+//to convert JSON to XML using Newtonsoft.Json's method
+namespace XMLtoJSONAndJSONToXML
+{
+    class Program
+    {
+        static string jan()
+        {
+            string json = @"{
+  '?xml': {
+    '@version': '1.0',
+    '@standalone': 'no'
+  },
+  'books': {
+    'book': [
+      {
+        'Name' :'Chris Jordan',
+        'Age' :'25',
+        'Nationality':'US'
+      },
+      {
+        'Name' :'Hari Haran',
+        'Age' :'55',
+        'Nationality':'IN'
+      },
+      {
+        'Name' :'Roshan Rao',
+        'Age' :'25',
+        'Nationality':'IN'
+      }
+    ]
+  }
+}";
+            return json;
+        }
+        static void Main(string[] args)
+        {
+            
+            XmlDocument doc = (XmlDocument)JsonConvert.DeserializeXmlNode(jan());
+
+            Console.WriteLine(doc.OuterXml);
+        }
+    }
+}
